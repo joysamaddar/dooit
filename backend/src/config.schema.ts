29376734c12a1bbe11d95bd524b4dfa938727358
Joy Samaddar <joysamaddar123@gmail.com>
2023-03-22 03:41:09 +0530

    Updated config schema validation file

--- backend/src/config.schema.ts
+++ backend/src/config.schema.ts
@@ -4,4 +4,5 @@ export const configSchema = Joi.object({
   STAGE: Joi.string().required(),
   PORT: Joi.number().default(3000),
   MONGODB_DATABASE_URL: Joi.string().required(),
+  JWT_SECRET: Joi.string().required(),
 });
