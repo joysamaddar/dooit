d20c7b163b05a12d6d753982f9ef984cc46a3554
Joy Samaddar <joysamaddar123@gmail.com>
2023-03-24 02:15:42 +0530

    Added frontend boilerplate code

--- backend/src/config.schema.ts
+++ backend/src/config.schema.ts
@@ -2,7 +2,7 @@ import * as Joi from 'joi';
 
 export const configSchema = Joi.object({
   STAGE: Joi.string().required(),
-  PORT: Joi.number().default(3000),
+  PORT: Joi.number().default(4000),
   MONGODB_DATABASE_URL: Joi.string().required(),
   JWT_SECRET: Joi.string().required(),
 });
